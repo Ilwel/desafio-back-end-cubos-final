cf7b21f8fafc3613e932b853e3da32542c23bb2f
ilwel <ilwelisaac@hotmail.com>
2021-09-26 02:54:50 -0300

    verifyHash function

--- utils/hashFunctions.js
+++ utils/hashFunctions.js
@@ -1,7 +1,9 @@
 const bcrypt = require('bcrypt');
 
 const createHash = async (password) => await bcrypt.hash(password, 10);
+const verifyHash = async (password, hash) => await bcrypt.compare(password, hash);
 
 module.exports = {
   createHash,
+  verifyHash
 }
