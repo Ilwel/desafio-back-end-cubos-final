c5661e6a22f1d7f8bb3f044f49b09a17d4f85198
ilwel <ilwelisaac@hotmail.com>
2021-09-26 02:57:07 -0300

    /login

--- router.js
+++ router.js
@@ -3,5 +3,6 @@ const router = express();
 const users = require('./controllers/users');
 
 router.post('/register', users.postRegistration);
+router.post('/login', users.postLogin);
 
 module.exports = router;
