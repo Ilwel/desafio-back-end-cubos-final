d52999cf398dac093c7531675b81408ca80c39b3
ilwel <ilwelisaac@hotmail.com>
2021-09-26 19:47:43 -0300

    /profile (get/put)

--- router.js
+++ router.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express();
 const users = require('./controllers/users');
+const profile = require('./controllers/profile');
+const checkLogin = require('./middlewares/checkLogin');
 
 router.post('/register', users.postRegistration);
 router.post('/login', users.postLogin);
+
+router.get('/profile', checkLogin, profile.getProfile);
+router.put('/profile', checkLogin, profile.putProfile);
 
 module.exports = router;
