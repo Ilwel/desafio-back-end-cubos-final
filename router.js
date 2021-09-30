6158416cd6fb5b3b8d347082ef4ede82dfe8761b
unknown <gabitelhado@gmail.com>
2021-09-30 18:30:46 -0300

    add router client/register

--- router.js
+++ router.js
@@ -2,12 +2,17 @@ const express = require('express');
 const router = express();
 const users = require('./controllers/users');
 const profile = require('./controllers/profile');
+const clients = require('./controllers/clients');
 const checkLogin = require('./middlewares/checkLogin');
 
 router.post('/register', users.postRegistration);
 router.post('/login', users.postLogin);
 
-router.get('/profile', checkLogin, profile.getProfile);
-router.put('/profile', checkLogin, profile.putProfile);
+router.use(checkLogin); 
+
+router.get('/profile', profile.getProfile);
+router.put('/profile', profile.putProfile);
+
+router.post('/client/register', clients.registerClient);
 
 module.exports = router;
