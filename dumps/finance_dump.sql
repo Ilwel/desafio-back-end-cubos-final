c0acddb6794d407e18938d32f050f731e245fb98
unknown <gabitelhado@gmail.com>
2021-09-30 18:31:33 -0300

    add client table

--- dumps/finance_dump.sql
+++ dumps/finance_dump.sql
@@ -8,3 +8,19 @@ create table if not exists users(
  	cpf varchar(11),
   	phone varchar(11)
 );
+
+drop table if exists clients;
+
+create table if not exists clients(
+	id serial primary key,
+	name varchar not null,
+	email text not null unique,
+	cpf varchar (11) not null,
+	phone varchar (11) not null,
+	zip_code varchar (8),
+	adress text, 
+	complement text, 
+	district text, 
+	city text, 
+	state text
+);
