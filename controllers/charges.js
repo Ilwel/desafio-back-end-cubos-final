199f0f31f714a487ca93d51bf7ace226a573ef29
unknown <gabitelhado@gmail.com>
2021-10-08 22:15:24 -0300

    format code

--- controllers/charges.js
+++ controllers/charges.js
@@ -3,15 +3,15 @@ const { chargeSchema } = require('../utils/yupSchemas');
 const dateFns = require('date-fns');
 
 const registerCharge = async (req, res) => {
-  
+
   const { client_id, description, paid, value, due_date } = req.body;
 
   try {
-    
+
     await chargeSchema.validate(req.body);
 
     const chargeRegistration = await db('charges')
-      .insert({client_id, description, paid, value, due_date}); 
+      .insert({ client_id, description, paid, value, due_date });
 
     if (chargeRegistration.rowCount > 0) {
       return res.status(200).json('cobrança cadastrada');
@@ -23,7 +23,7 @@ const registerCharge = async (req, res) => {
     }
 
   } catch (error) {
-    
+
     return res.status(400 || error.status).json(error.message);
   }
 
@@ -34,27 +34,27 @@ const getCharges = async (req, res) => {
   try {
 
     const joinChargeClient = await db('charges')
-    .join('clients', 'charges.client_id', 'clients.id' )
-    .select('charges.id', 'clients.name', 'charges.description', 'charges.value', 'charges.paid', 'charges.due_date');
+      .join('clients', 'charges.client_id', 'clients.id')
+      .select('charges.id as charge_id', 'clients.name', 'charges.description', 'charges.value', 'charges.paid', 'charges.due_date');
 
     function changeStatus(charges) {
 
       return charges.map(charge => {
-          const isDateAfter = dateFns.isAfter(charge.due_date, new Date());
-          const isDateBefore = dateFns.isBefore(charge.due_date, new Date());
-  
-          if(!charge.paid && isDateAfter){
-              charge.status = 'pendente'
-          } else if(!charge.paid && isDateBefore){
-              charge.status = 'vencido'
-          } else {
-              charge.status = 'pago'
-          }
-          delete charge.paid;
-          return charge
+        const isDateAfter = dateFns.isAfter(charge.due_date, new Date());
+        const isDateBefore = dateFns.isBefore(charge.due_date, new Date());
+
+        if (!charge.paid && isDateAfter) {
+          charge.status = 'pendente'
+        } else if (!charge.paid && isDateBefore) {
+          charge.status = 'vencido'
+        } else {
+          charge.status = 'pago'
+        }
+        delete charge.paid;
+        return charge
       })
-  };
-    
+    };
+
     const charges = changeStatus(joinChargeClient);
 
     return res.status(200).json(charges);
@@ -67,6 +67,6 @@ const getCharges = async (req, res) => {
 }
 
 module.exports = {
-    registerCharge,
-    getCharges
-  }
+  registerCharge,
+  getCharges
+}
